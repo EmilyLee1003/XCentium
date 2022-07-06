c1b002ef4e77e6beabac7096ea0786cca877394b
Emily Lee <emlee088@gmail.com>
2022-07-06 13:33:50 -0700

    server set up

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+var mysql = require('mysql');
 const port = 3000
 
 app.get('/', (req, res) => {
@@ -9,3 +10,14 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
 })
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected to the database!");
+  });
